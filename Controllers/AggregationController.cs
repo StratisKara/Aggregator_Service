d4c73030397d07288010beb85fd99fe66555d988
e.karampasis <e.karampasis@qubiteq.gr>
2025-05-02 12:14:06 +0300

    error handling, fallback impl

--- Controllers/AggregationController.cs
+++ Controllers/AggregationController.cs
@@ -1,3 +1,4 @@
+using ApiAggregatorService.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiAggregatorService.Controllers
@@ -23,23 +24,60 @@ namespace ApiAggregatorService.Controllers
         public async Task<IActionResult> GetAggregatedData(
             [FromQuery] string city = "Athens",
             [FromQuery] string newskeyword = "technology",
-            [FromQuery] string githubkeyword = "dontet")
+            [FromQuery] string githubkeyword = "dotnet",
+            [FromQuery] DateTime? publishedAfter = null,
+            [FromQuery] string sortBy = "date_desc")
         {
 
-            var weatherTask = _weatherService.GetWeatherInfoAsync(city);
-            var newsTask = _newsService.GetNewsArticlesAsync(newskeyword);
-            var githubTask = _gitHubService.SearchReposAsync(githubkeyword);
+            var weatherTask = SafeCall(() => _weatherService.GetWeatherInfoAsync(city));
+            var newsTask = SafeCall(() => _newsService.GetNewsArticlesAsync(newskeyword));
+            var githubTask = SafeCall(() => _gitHubService.SearchReposAsync(githubkeyword));
 
             await Task.WhenAll(weatherTask, newsTask, githubTask);
 
+            var weather = weatherTask.Result ?? new WeatherInfo { Temperature = 0, Description = "Unavailable" };
+            var newsArticles = newsTask.Result ?? new List<NewsArticle>();
+            var githubRepos = githubTask.Result ?? new List<GitHubRepo>();
+
+
+
+            if (publishedAfter.HasValue)
+            {
+                newsArticles = newsArticles
+                    .Where(n => n.PublishedAt > publishedAfter.Value)
+                    .ToList();
+            }
+
+            newsArticles = sortBy switch
+            {
+                "date_asc" => newsArticles.OrderBy(n => n.PublishedAt).ToList(),
+                "date_desc" => newsArticles.OrderBy(n => n.PublishedAt).ToList(),
+                _ => newsArticles
+            };
+
+
+
             var result = new
             {
-                Weather = weatherTask.Result,
-                News = newsTask.Result.Take(5),
-                GitHubRepo = githubTask.Result.Take(5)
+                Weather = weather,
+                News = newsArticles.Take(5),
+                GitHubRepo = githubRepos.Take(5)
             };
 
             return Ok(result);
+        }
+
+        public async Task<T?> SafeCall<T>(Func<Task<T>> apiCall)
+        {
+            try
+            {
+                return await apiCall();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"API call failed with message: {ex.Message}");
+                return default;
+            }
         }
     }    
 }
